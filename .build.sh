0232d0d8385361fca89b83c5c5d1afe919474554
dynomite567 <17055345+dynomite567@users.noreply.github.com>
2019-09-23 16:55:43 -0700

    Variables Fixed

--- .build.sh
+++ .build.sh
@@ -3,7 +3,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 archarchitectures="arm arm64 amd64"
 platforms=""
-DOCKER_TAG="latest"
+images=""
 
 for arch in $architectures
 do
@@ -14,13 +14,13 @@ done
 platforms=${platforms::-1}
 
 buildctl build --frontend dockerfile.v0 \
-        --frontend-opt platform=${platforms} \
-        --frontend-opt filename=./Dockerfile \
-        --exporter image \
-        --exporter-opt name=docker.io/$DOCKER_USER/$DOCKER_IMAGE:latest \
-        --exporter-opt push=true \
         --local dockerfile=. \
-        --local context=.
+        --local context=. \
+        --exporter image \
+        --exporter-opt name=docker.io/$DOCKER_USERNAME/$DOCKER_IMAGE:latest \
+        --exporter-opt push=true \
+        --frontend-opt platform=$platforms \
+        --frontend-opt filename=./Dockerfile 
 
 for arch in $architectures
 do
@@ -29,7 +29,7 @@ do
       --local dockerfile=. \
       --local context=. \
       --exporter image \
-      --exporter-opt name=docker.io/$DOCKER_USER/$DOCKER_IMAGE:latest \
+      --exporter-opt name=docker.io/$DOCKER_USERNAME/$DOCKER_IMAGE:latest \
       --exporter-opt push=true \
       --frontend-opt platform=linux/$arch \
       --frontend-opt filename=./Dockerfile &
